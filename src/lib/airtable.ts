a54a781318d69c3e571b12dfd9f10d4d064e5c20
VinnyChase28 <vincentgauthier068@gmail.com>
2024-05-07 16:10:24 -0700

    move to airtable file

--- src/lib/airtable.ts
+++ src/lib/airtable.ts
@@ -1,6 +1,10 @@
 import { FieldSet, Table } from "airtable";
-import { base } from "~/lib/utils";
+import Airtable from "airtable";
 import { TRPCError } from "@trpc/server";
+
+export const base = new Airtable({ apiKey: process.env.AIRTABLE_PAT }).base(
+  process.env.AIRTABLE_BASE_ID ?? "",
+);
 
 export class AirtableClient<T extends FieldSet> {
   private table: Table<T>;
