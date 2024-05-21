9eae2656b272fe4d4fb80dfa9bd3c48b3f11cf29
VinnyChase28 <vincentgauthier068@gmail.com>
2024-05-20 17:02:09 -0700

    update links

--- src/app/_components/navigation/top-nav.tsx
+++ src/app/_components/navigation/top-nav.tsx
@@ -5,7 +5,7 @@ export default function Topbar() {
   return (
     <header className="fixed left-0 top-0 z-50 flex h-16 w-full items-center justify-between bg-white shadow-sm dark:bg-gray-950 dark:text-gray-50 sm:px-6 lg:px-8">
       <div className="flex h-full items-center">
-        <Link className="ml-2 text-2xl font-bold" href="#">
+        <Link className="ml-2 text-2xl font-bold" href="/">
           <Image
             src="/mcw-logo.png"
             alt="Left Logo"
@@ -21,7 +21,7 @@ export default function Topbar() {
         </span>
       </div>
       <div className="flex h-full items-center">
-        <Link className="ml-2 text-2xl font-bold" href="#">
+        <Link className="ml-2 text-2xl font-bold" href="/">
           <Image
             src="/cumulus-logo.png"
             alt="Right Logo"
